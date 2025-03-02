c7785f42947caef9e9c58d9d9b539a6a072543f5
hussein12347 <162142849+hussein12347@users.noreply.github.com>
2025-03-03 01:30:19 +0200

    create database test.db

--- lib/core/database/sqlite/my_sql_database.dart
+++ lib/core/database/sqlite/my_sql_database.dart
@@ -1,18 +1,45 @@
+import 'package:sqflite/sqflite.dart';
+
 import 'crud.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart' as sqlDatabase;
-class MySqlDataBase extends CRUD{
+
+class MySqlDataBase extends CRUD {
+  final String _userTable = "users";
+  final String _userColumnID = "id";
+  final String _userColumnUsername = "username";
+  final String _productTable = "products";
+  final String _productColumnID = "product_id";
+  final String _productColumnName = "product_name";
+  final String _productColumnPrice = "product_price";
+  final String _productColumnCount = "product_count";
+  final String _salesTable = "sales";
+  final String _salesColumnID = "sales_id";
+  final String _salesColumnUsername = "sales_username";
+  final String _salesColumnProductName = "sales_product_name";
+
   initDatabase() async {
     String databasePath = await sqlDatabase.getDatabasesPath();
 
-    String DatabaseName="management.db";
-    String realDatabasePath=join(databasePath,DatabaseName);
+    String databaseName = "management.db";
+    String realDatabasePath = join(databasePath, databaseName);
     int version = 1;
-    sqlDatabase.openDatabase(realDatabasePath,version: version,onCreate: (db, version) {
-
-    },);
-
+    sqlDatabase.openDatabase(
+      realDatabasePath,
+      version: version,
+      onCreate: _onCreate,
+    );
   }
+
+  _onCreate(Database db, int version) async {
+    await db.execute(
+        "CREATE TABLE $_userTable($_userColumnID INTEGER , $_userColumnUsername TEXT)");
+    await db.execute(
+        "CREATE TABLE $_productTable($_productColumnID INTEGER , $_productColumnName TEXT ,$_productColumnPrice REAL ,$_productColumnCount  INTEGER )");
+    await db.execute(
+        "CREATE TABLE $_salesTable($_salesColumnID INTEGER  ,$_salesColumnProductName TEXT , $_salesColumnUsername TEXT )");
+  }
+
   @override
   Future<void> delete() {
     // TODO: implement delete
@@ -36,5 +63,4 @@ class MySqlDataBase extends CRUD{
     // TODO: implement update
     throw UnimplementedError();
   }
-
 }
